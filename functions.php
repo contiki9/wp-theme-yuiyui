f419a7f764286789946b9edc7528f27be9412edc
hideokamoto <hide.okamoto@digitalcube.jp>
2017-01-13 13:05:30 +0900

    change function name as unique

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 //Load CSS of parent theme and child theme
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-function theme_enqueue_styles() {
+add_action( 'wp_enqueue_scripts', 'yuiyui_enqueue_styles' );
+function yuiyui_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style') );
 }
