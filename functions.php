29da497ca98c55af67854c01dc6414f4185c2acc
hideokamoto <hide.okamoto@digitalcube.jp>
2017-01-13 13:06:32 +0900

    should not use php?>

--- functions.php
+++ functions.php
@@ -5,4 +5,3 @@ function yuiyui_enqueue_styles() {
 	wp_enqueue_style( 'parent-style', path_join( get_template_directory_uri(), 'style.css' ) );
 	wp_enqueue_style( 'child-style', path_join( get_stylesheet_directory_uri(), 'style.css' ) , array('parent-style') );
 }
-?>
