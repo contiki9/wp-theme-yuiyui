a1779fa2231e16a84df6b1aa0d7be633e688df8b
hideokamoto <hide.okamoto@digitalcube.jp>
2017-01-19 20:51:56 +0900

    allow wpcs

--- functions.php
+++ functions.php
@@ -1,7 +1,17 @@
 <?php
-//Load CSS of parent theme and child theme
+/**
+ * Theme yuiyui's functions
+ *
+ * @package yuiyui
+ **/
+
+// Load CSS of parent theme and child theme.
 add_action( 'wp_enqueue_scripts', 'yuiyui_enqueue_styles' );
+
+/**
+ * Enqueue stylesheet.
+ **/
 function yuiyui_enqueue_styles() {
 	wp_enqueue_style( 'parent-style', path_join( get_template_directory_uri(), 'style.css' ) );
-	wp_enqueue_style( 'child-style', path_join( get_stylesheet_directory_uri(), 'style.css' ) , array('parent-style') );
+	wp_enqueue_style( 'child-style', path_join( get_stylesheet_directory_uri(), 'style.css' ) , array( 'parent-style' ) );
 }
